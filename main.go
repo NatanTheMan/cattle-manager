6a4863e821452c11e26e754186d135aadde6add2
Natan <natanscosta7@gmail.com>
2025-02-22 16:18:45 -0300

    feat: custom logger

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/NatanTheMan/cattle-manager/router"
+import (
+	"github.com/NatanTheMan/cattle-manager/config"
+	"github.com/NatanTheMan/cattle-manager/router"
+)
+var (logger * config.Logger)
 
 func main() {
+  logger=config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+    logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
