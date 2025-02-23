c3b7b86413570cb797428ab589e8a2befca5624c
Natan <natanscosta7@gmail.com>
2025-02-23 11:03:12 -0300

    feat: add delete cattle handler

--- handler/delete_cattle.go
+++ handler/delete_cattle.go
@@ -1,11 +1,30 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/NatanTheMan/cattle-manager/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func DeleteCattleHandler(ctx *gin.Context)  {
-  ctx.JSON(http.StatusOK, gin.H{"msg":"sdlfkjsakdj"})
+func DeleteCattleHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParamter").Error())
+		return
+	}
+	cattle := schemas.Cattle{}
+
+	if err := db.First(&cattle, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("cattle with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&cattle).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting cattle with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "delete-cattle", cattle)
 }
