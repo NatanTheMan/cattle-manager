074ab144a8dbfca3faaac76c02c809c3e221944f
Natan <natanscosta7@gmail.com>
2025-02-23 11:46:08 -0300

    feat: add show cattle handler logic

--- handler/show_cattle.go
+++ handler/show_cattle.go
@@ -3,9 +3,22 @@ package handler
 import (
 	"net/http"
 
+	"github.com/NatanTheMan/cattle-manager/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func ShowCattleHandler(ctx *gin.Context)  {
-  ctx.JSON(http.StatusOK, gin.H{"msg":"sdlfkjsakdj"})
+func ShowCattleHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "string").Error())
+		return
+	}
+
+	cattle := schemas.Cattle{}
+	if err := db.First(&cattle,id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "cattle not found")
+    return
+	}
+
+	sendSuccess(ctx, "show-cattle", cattle)
 }
