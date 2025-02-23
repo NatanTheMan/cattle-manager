e1281a5cb9796248026315a15fdee2379deb7576
Natan <natanscosta7@gmail.com>
2025-02-23 16:53:55 -0300

    feat: ensure that's impossible to create two cattle with same earring

--- handler/create_cattle.go
+++ handler/create_cattle.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -21,6 +22,7 @@ import (
 // @Failure 500 {object} ErrorResponse
 // @Router /cattle [post]
 func CreateCattleHandler(ctx *gin.Context) {
+  cattle := schemas.Cattle{}
 	request := CreateCattleRequest{}
 
 	ctx.BindJSON(&request)
@@ -30,8 +32,12 @@ func CreateCattleHandler(ctx *gin.Context) {
 		sendError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
+  if err:=db.Find(&cattle, "earring = ?", request.Earring).Error; err == nil {
+    sendError(ctx,http.StatusBadRequest, fmt.Sprintf("a cattle with earring: %s already exists", request.Earring))
+    return
+  }
 
-	cattle := schemas.Cattle{
+	cattle = schemas.Cattle{
 		Name:    request.Name,
 		Earring: request.Earring,
 		Gender:  strings.ToUpper(request.Gender),
