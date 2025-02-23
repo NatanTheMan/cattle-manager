2203a92b1f6b0f6731242eaec81945325b894659
Natan <natanscosta7@gmail.com>
2025-02-23 11:46:28 -0300

    feat: add update cattle handler logic

--- handler/update_cattle.go
+++ handler/update_cattle.go
@@ -3,9 +3,42 @@ package handler
 import (
 	"net/http"
 
+	"github.com/NatanTheMan/cattle-manager/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func UpdateCattleHandler(ctx *gin.Context)  {
-  ctx.JSON(http.StatusOK, gin.H{"msg":"sdlfkjsakdj"})
+func UpdateCattleHandler(ctx *gin.Context) {
+	request := UpdateCattleRequest  {}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	cattle := schemas.Cattle{}
+	if err := db.First(&cattle, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "cattle not found")
+		return
+	}
+
+	if request.Name != "" {
+		cattle.Name = request.Name
+	}
+
+	if err := db.Save(&cattle).Error; err != nil {
+		logger.Errorf("error updating cattle: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updating cattle")
+		return
+	}
+	sendSuccess(ctx, "update-cattle", cattle)
+
 }
